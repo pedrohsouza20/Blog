3d75de536a761e192714f91f4c70bb8f4df602db
Pedro <contato.pedromoc@gmail.com>
2022-04-03 10:06:29 -0300

    criados controllers de artigos e de categorias

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -16,6 +19,8 @@ connection
     console.log(error);
   });
 
+app.use("/", categoriesController);
+app.use("/", articlesController);
 app.get("/", (req, res) => {
   res.render("index");
 });
