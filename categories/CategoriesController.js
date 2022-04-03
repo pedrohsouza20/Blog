28bd76a1cc76f7c6a24d7b610b517dc36b73b443
Pedro <contato.pedromoc@gmail.com>
2022-04-03 13:08:17 -0300

    adicionados ao controller de categorias: create, read e delete

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,12 +1,54 @@
 const express = require("express");
+const { default: slugify } = require("slugify");
+const Category = require("./Category");
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("Rota de categorias");
+router.get("/admin/categories/new", (req, res) => {
+  res.render("admin/categories/new");
 });
 
-router.get("/admin/categories/new", (req, res) => {
-  res.send("Rota de nova categoria");
+router.post("/categories/save", (req, res) => {
+  let title = req.body.title;
+
+  if (title) {
+    Category.create({
+      title: title,
+      slug: slugify(title, { lower: true }),
+    })
+      .then(() => {
+        res.redirect("/admin/categories");
+      })
+      .catch((error) => {
+        console.log("Erro ao cadastrar nova categoria", error);
+      });
+  } else {
+    res.redirect("/admin/categories/new");
+  }
+});
+
+router.get("/admin/categories", (req, res) => {
+  Category.findAll({}).then((categories) => {
+    res.render("admin/categories/index", { categories: categories });
+  });
+});
+
+router.post("/categories/delete", (req, res) => {
+  let id = req.body.id;
+  if (id != null) {
+    if (!isNaN(id)) {
+      Category.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/categories");
+      });
+    } else {
+      res.redirect("/admin/categories");
+    }
+  } else {
+    res.redirect("/admin/categories");
+  }
 });
 
 module.exports = router;
