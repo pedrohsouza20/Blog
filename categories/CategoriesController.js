b910f9f32ecf41f35a7f02045e8d177658e7271e
Pedro <contato.pedromoc@gmail.com>
2022-04-03 14:58:04 -0300

    criada página de edição de categorias e rota de update para edição

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const res = require("express/lib/response");
+const { redirect } = require("express/lib/response");
 const { default: slugify } = require("slugify");
 const Category = require("./Category");
 const router = express.Router();
@@ -49,6 +51,44 @@ router.post("/categories/delete", (req, res) => {
   } else {
     res.redirect("/admin/categories");
   }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  let id = req.params.id;
+
+  if (isNaN(id)) {
+    res.redirect("/admin/categories");
+    return;
+  }
+
+  Category.findByPk(id)
+    .then((category) => {
+      console.log(category);
+      if (category != undefined) {
+        res.render("admin/categories/edit", { category: category });
+      } else {
+        res.redirect("/admin/categories");
+      }
+    })
+    .catch((error) => {
+      res.redirect("/admin/categories");
+    });
+});
+
+router.post("/categories/update", (req, res) => {
+  let id = req.body.id;
+  let title = req.body.title;
+
+  Category.update(
+    { title: title, slug: slugify(title, { lower: true }) },
+    {
+      where: {
+        id: id,
+      },
+    }
+  ).then(() => {
+    res.redirect("/admin/categories");
+  });
 });
 
 module.exports = router;
