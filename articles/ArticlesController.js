dd5007285a0369d26ca4c120f286d55f4dfc4f08
Pedro <contato.pedromoc@gmail.com>
2022-04-03 17:42:38 -0300

    criada rota e view de deleção e criação de artigos e adicionada tradução pt_BR ao tinymce

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,60 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/articles", (req, res) => {
-  res.send("Rota de artigos");
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
+
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{ model: Category }],
+  }).then((articles) => {
+    res.render("admin/articles/index", { articles: articles });
+  });
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.render("admin/articles/new");
+  Category.findAll().then((categories) => {
+    res.render("admin/articles/new", { categories: categories });
+  });
+});
+
+router.post("/article/save", (req, res) => {
+  let title = req.body.title;
+  let body = req.body.body;
+  let categoryId = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title, { lower: true }),
+    body: body,
+    categoryId: categoryId,
+  })
+    .then(() => {
+      res.redirect("/admin/articles");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.post("/articles/delete", (req, res) => {
+  let id = req.body.id;
+  if (id != null) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/admin/articles");
+      });
+    } else {
+      res.redirect("/admin/articles");
+    }
+  } else {
+    res.redirect("/admin/articles");
+  }
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize("blog-with-cms", "root", "19372855", {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00"
 });
 
 module.exports = connection;
